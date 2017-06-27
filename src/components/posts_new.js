1c02464c21a79aac5cfc39f396c107c1e0d9f082
Dmitry Ryumkin <riumkin@gmail.com>
2017-06-27 11:30:16 +0300

    add form validation and submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render() {
-    return(
-      <div>
-        Posts New!
+
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+        className="form-control"
+        type="text"
+          {...field.input}
+        />
+        {field.meta.error}
       </div>
+    )
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return(
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field 
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field 
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field 
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title more that 3 charactes!"
+  }
+  if (!values.categories) {
+    errors.categories = "Enter category"
+  }
+  if (!values.content) {
+    errors.content = "enter content"
+  }
+  
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
