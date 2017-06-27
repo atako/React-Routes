07379d3b1e0c33f00990df6ba37534c805879f4a
Dmitry Ryumkin <riumkin@gmail.com>
2017-06-27 12:35:09 +0300

    add post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
+
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=dima';
 
@@ -13,3 +15,13 @@ export function fetchPosts() {
   };
 }
 
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+  
+}
